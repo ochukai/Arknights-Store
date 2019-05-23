05eba2d08d7f7920ad1164908cf6b8e8a9714355
ochukai <sex.xiaoyee@gmail.com>
2019-05-23 10:57:43 +0800

    忘了改这里。。。

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,18 @@
+/* config-overrides.js */
+
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+const rewireDefinePlugin = require('react-app-rewire-define-plugin');
+
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
+    // config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#1DA57A" },
+    })(config, env);
+
+    config = rewireDefinePlugin(config, env, {
+        __DEV__: false
+    });
+
+    return config;
 };
