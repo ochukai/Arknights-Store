40b68d0e8ae488703caf754a56b0e7df986540e9
Oliver <sex.xiaoyee@gmail.com>
2019-05-23 22:06:28 +0800

    终于可以运行了，试一下看看页面

---
+++ build/precache-manifest.6a74662b3fe4f61a8447a2c6ec733029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8406aac5e47c397f62043a3a541bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a28f16b9a2b635cf9e5",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9a28f16b9a2b635cf9e5",
+    "url": "/static/js/main.094d6a15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,12 @@
 /* config-overrides.js */
 
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
 const rewireDefinePlugin = require('react-app-rewire-define-plugin');
 
 module.exports = function override(config, env) {
-    // config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-    config = rewireLess.withLoaderOptions({
-        modifyVars: { "@primary-color": "#1DA57A" },
-    })(config, env);
 
-    config = rewireDefinePlugin(config, env, {
-        __DEV__: false
-    });
+  config = rewireDefinePlugin(config, env, {
+    __DEV__: false
+  });
 
-    return config;
+  return config;
 };
