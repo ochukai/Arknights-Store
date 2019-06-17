2303c48f52b3042ce578b3a5f0709d02cc8f5ee5
Oliver <sex.xiaoyee@gmail.com>
2019-06-18 00:16:19 +0800

    0.8.0 - 仓库与计算器可以联动了~

--- src/pages/material/MaterialTreeNode.js
+++ src/pages/material/MaterialTreeNode.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './index.scss';
 
+import memoizeOne from 'memoize-one';
 // import _ from 'lodash';
-// import store from 'store';
+import store from 'store';
 
 import { Material, Counter } from '../../components';
+
+import allItems from '../../data/items/items.json';
+
+export const dropRateMaps = {
+  5: '罕见',
+  4: '小概率',
+  3: '概率',
+  2: '大概率',
+  1: '固定',
+};
 
 export default class MaterialTreeNode extends Component {
 
@@ -13,18 +24,31 @@ export default class MaterialTreeNode extends Component {
     this.props.onCounterChange(item, value);
   };
 
-  renderChildNode(item) {
-    const { count, req = 0, name, id } = item;
+  renderChildNode(item, drops) {
+    const {
+      count,
+      req = 0,
+      name,
+      id,
+      store = 0
+    } = item;
     const total = count * req;
-    // const storeCount = store.get(id);
 
     let descSpan;
-    // if (storeCount && req) {
-    //   const result = storeCount > total ? 0 : total - storeCount;
-    //   descSpan = <span><strong>{count}</strong> * {req} = {total} - {storeCount} = {result}</span>
-    // } else
-    if (req) {
-      descSpan = <span><strong>{count}</strong> * {req} = {total}</span>
+    if (store && req) {
+      const result = store > total ? 0 : total - store;
+      const style = {};
+      if (result === 0) {
+        style.textDecoration = 'line-through';
+      }
+
+      descSpan = (
+        <span style={style}>
+          <strong>{count}</strong> * {req} = {total} - {store}(库存) = <strong>{result}</strong>
+        </span>
+      );
+    } else if (req) {
+      descSpan = <span><strong>{count}</strong> * {req} = <strong>{total}</strong></span>
     } else {
       descSpan = <span><strong>{count}</strong></span>
     }
@@ -36,21 +60,102 @@ export default class MaterialTreeNode extends Component {
           <h2>{name}</h2>
           {descSpan}
         </div>
+        {this.renderDrops(drops)}
       </div>
+    );
+  }
+
+  findDrops = memoizeOne((id) => {
+    const item = allItems.filter(ai => ai.id === id)[0];
+    if (item) {
+      return item.drops.map(drop => {
+        const { occPer, stage = {} } = drop;
+        return {
+          occPer,
+          code: stage.code
+        }
+      });
+    }
+
+    return [];
+  });
+
+  renderDrops(drops = []) {
+    if (drops.length === 0) {
+      return;
+    }
+
+    return (
+      <div className="material-drops">
+        {drops.map((drop, index) => {
+          const { code, occPer } = drop;
+          const style = {};
+          switch(occPer) {
+            case 5:
+              style.color = 'red';
+              break;
+            case 4:
+              style.color = 'orange';
+              break;
+            case 1:
+              style.color = 'green';
+              break;
+          }
+
+          return (
+            <p
+              key={index}
+              style={style}
+            >
+              {drop.code}（{dropRateMaps[drop.occPer]}）
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
+
+  renderRootCount(item) {
+    const { req = 0, store = 0 } = item;
+
+    if (store === 0) {
+      return;
+    }
+
+    if (req === 0) {
+      return <span>{store}(库存)</span>;
+    }
+
+    const need = req - store;
+    const result = need < 0 ? 0 : need;
+    const style = {};
+    if (result === 0) {
+      style.textDecoration = 'line-through';
+    }
+
+    return (
+      <span style={style}>
+        {req} - {store}(库存) = <strong>{result}</strong>
+      </span>
     );
   }
 
   render() {
     const { item, level } = this.props;
+    const drops = this.findDrops(item.id);
     if (level > 0) {
-      return this.renderChildNode(item);
+      return this.renderChildNode(item, drops);
     }
 
     return (
       <div className="root-node-wrapper">
         <Material id={item.id} noCount size={60} />
-        <h1>{item.name}</h1>
+        <div className="mate-wrapper">
+          <h1>{item.name}</h1>
+          {this.renderRootCount(item)}
+        </div>
         <Counter onChange={this.handleCounterChange} />
+        {this.renderDrops(drops)}
       </div>
     );
   }
--- src/pages/material/index.js
+++ src/pages/material/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 
 import classNames from 'classnames';
 import _ from 'lodash';
+import store from 'store';
 
 import materialItems from '../../data/items/material_items.json';
 
@@ -25,29 +26,64 @@ export default class AKMaterial extends Component {
     };
   }
 
-  updateInnerCount(item, count) {
-    if (item.children.length === 0) {
-      return;
-    }
-
-    item.children.forEach(ic => {
-      ic.req = count;
-      this.updateInnerCount(ic, count * ic.count);
-    });
+  componentDidMount() {
+    this.updateData();
   }
 
-  computeCount = (item, result) => {
-    const { id, count = 1, req = 0, rarity, children = [] } = item;
-    if (!req) { // 没有需求就算了
-      return;
+  updateData = () => {
+    const { items } = this.state;
+
+    const newItems = _.clone(items);
+    newItems.forEach(item => {
+      const storeCount = store.get(item.id);
+      item.store = storeCount || 0;
+      item.count = 1;
+    });
+
+    this.setState({
+      items: newItems
+    });
+  };
+
+  updateInnerCount(item, count) {
+    const storeCount = store.get(item.id);
+    item.store = storeCount || 0;
+    item.req = count;
+
+    let needCount = count * item.count;
+    if (needCount > 0) {
+      if (storeCount >= needCount) { // 仓库富裕，子合成材料不需要算了。
+        needCount = 0;
+      } else {
+        needCount -= item.store;
+      }
     }
 
-    console.log(item);
+    if (item.children.length > 0) {
+      item.children.forEach(ic => this.updateInnerCount(ic, needCount));
+    }
+  }
+
+  computeRequestCount = (item, result) => {
+    const {
+      id,
+      count = 1,
+      req = 0,
+      rarity,
+      children = [],
+      store = 0
+    } = item;
+
+    const realRequestCount = req * count;
+    const requestExcludeStore = realRequestCount - store;
+    if (requestExcludeStore <= 0) { // 库存够了
+      return;
+    }
 
     let exist = false;
     result.forEach(dummy => {
-      if (dummy.id === item.id) {
-        dummy.count += req * count;
+      if (dummy.id === id) {
+        dummy.count += requestExcludeStore;
         exist = true;
       }
     });
@@ -55,7 +91,7 @@ export default class AKMaterial extends Component {
     if (!exist) {
       result.push({
         id: id,
-        count: req * count,
+        count: requestExcludeStore,
         rarity: rarity,
       });
 
@@ -63,7 +99,7 @@ export default class AKMaterial extends Component {
     }
 
     if (children.length > 0) {
-      children.forEach(foo => this.computeCount(foo, result));
+      children.forEach(foo => this.computeRequestCount(foo, result));
     }
   };
 
@@ -74,16 +110,15 @@ export default class AKMaterial extends Component {
     const newItems = _.clone(items);
     const cur = newItems.filter(i => i.id === id)[0];
     if (cur) {
-      cur.req = value;
       this.updateInnerCount(cur, value);
     }
 
     const newFlatItems = [];
     newItems.forEach(ni => {
       if (ni.children && ni.children.length > 0) {
-        ni.children.forEach(foo => this.computeCount(foo, newFlatItems));
+        ni.children.forEach(foo => this.computeRequestCount(foo, newFlatItems));
       } else {
-        this.computeCount(ni, newFlatItems);
+        this.computeRequestCount(ni, newFlatItems);
       }
     });
 
@@ -104,30 +139,29 @@ export default class AKMaterial extends Component {
   };
 
   renderSider(flatItems = []) {
-    console.log(flatItems, );
-
     const grouped = _.groupBy(flatItems, 'rarity');
 
     return (
-      <React.Fragment>
-        <h2>汇总</h2>
-        <hr />
-        {
-          Object.keys(grouped).reverse().map(key => {
-            const items = grouped[key];
-            return (
-              <div>
-                <h3>{key}</h3>
-                <div className="flat-item-group-wrapper">
-                  {items.map(fi => (
-                    <Material key={fi.id} id={fi.id} count={fi.count}/>
-                  ))}
+      <div className="summary-wrapper">
+        <h2>材料汇总</h2>
+        <div className="summary-group-wrapper">
+          {
+            Object.keys(grouped).reverse().map(key => {
+              const items = grouped[key];
+              return (
+                <div key={key}>
+                  <h3>{key}</h3>
+                  <div className="flat-item-group-wrapper">
+                    {items.map(fi => (
+                      <Material key={fi.id} id={fi.id} count={fi.count}/>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        }
-      </React.Fragment>
+              );
+            })
+          }
+        </div>
+      </div>
     );
   }
 
@@ -145,14 +179,14 @@ export default class AKMaterial extends Component {
               <Tree
                 data={item}
                 key={index}
-                clickToExpand={true}
+                clickToExpand={false}
                 treeNodeRender={this.treeNodeRender}
               />
             ))}
           </div>
         </Layout.Content>
 
-        <Layout.Sider width={400}>
+        <Layout.Sider>
           {flatItems.length > 0 && this.renderSider(flatItems)}
         </Layout.Sider>
       </Layout>
